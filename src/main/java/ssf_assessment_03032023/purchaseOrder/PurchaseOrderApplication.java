0582cf175d4d925c35a8afeedad8f74efa3cb6cc
LeeMeiLing <leem0060@outlook.com>
2023-03-03 09:25:24 +0800

    initial commit

---
+++ src/main/java/ssf_assessment_03032023/purchaseOrder/PurchaseOrderApplication.java
@@ -0,0 +1,13 @@
+package ssf_assessment_03032023.purchaseOrder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurchaseOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PurchaseOrderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ssf_assessment_03032023/purchaseOrder/PurchaseOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package ssf_assessment_03032023.purchaseOrder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurchaseOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
